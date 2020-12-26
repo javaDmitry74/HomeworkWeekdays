1f0aa17605d620e9d599589f423e5363e8bcb8d7
Irbis74 <42510936+Irbis74@users.noreply.github.com>
2020-12-26 13:37:42 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	NEW 1.iml,a\e\ae3c311c5e01dd52ad08bd3f3c5ea40f8f7151f3
+E
+src/Firstproject.java,b\a\ba5df179cb9c4f2d8c1b36bd5174fcf4e59720d0
---
+++ src/Firstproject.java
@@ -0,0 +1,17 @@
+public class Firstproject {
+    public static void main(String [] args){
+        int myint;
+          myint = 557;
+          short myshort = 3500;
+          long mylong = 6666666;
+
+          double myDOuble = 235.35;
+          float myFloat = 2362.4f;
+          char c = 'a';
+          boolean b = true;
+          byte myByte = 100; // -128-127
+
+          System.out.println(myFloat);
+    }
+
+}
